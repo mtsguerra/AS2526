e237ec4f8e151b6dfc051c6f162faf2de4833c6d
Matheus Guerra <mtsguerra9@gmail.com>
2026-03-10 10:32:41 +0000

    structuring

---
+++ src/maca/primeRobo.java
@@ -0,0 +1,17 @@
+package maca;
+import robocode.*;
+
+public class primeRobo extends Robot {
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+}
